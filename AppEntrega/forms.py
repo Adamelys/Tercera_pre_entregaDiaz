503adcce7c34b6d6f4ec969d5f3365a95ab86f4f
DIAZ RIVERO <ADIAZ@nacionservicios.com.ar>
2023-03-25 21:15:31 -0300

    creacion del CRUD para cursos

--- AppEntrega/forms.py
+++ AppEntrega/forms.py
@@ -1,8 +1,22 @@
 from django import forms
+from AppEntrega.models import Curso, Estudiantes, Profesor
 
-class CursoForm(forms.Form):
-    nombre = forms.CharField(min_length=3,max_length=40)
-    camada = forms.IntegerField(min_value=100)
+class CursoForm(forms.ModelForm):
+    # nombre = forms.CharField(min_length=3,max_length=40)
+    # camada = forms.IntegerField(min_value=100)
+    class Meta:
+        model = Curso
+        fields = "__all__"
+
+class Estudiantesform(forms.ModelForm):
+    class Meta:
+        model = Estudiantes
+        fields = "__all__"
+
+class Profesorform(forms.ModelForm):
+    class Meta:
+        model = Profesor
+        fields = "__all__"
 
 class BusquedaCursoForm(forms.Form):
     nombre = forms.CharField(min_length=3,max_length=40)
--- AppEntrega/urls.py
+++ AppEntrega/urls.py
@@ -3,8 +3,11 @@ from AppEntrega.views import *
 
 urlpatterns = [
     path('cursos',cursos,name="AppCoderCursos"),
+    path('cursos/crear',crear_curso,name="AppCoderCrearCurso"),
     path('buscar_curso',busqueda_curso,name="AppCoderBuscarCurso"),
-    path('curso/<nombre>/<camada>',crear_curso,name="AppCoderCurso"),
+    path('cursos/Editar/<camada>',editar_curso,name="AppCoderEditarCurso"),
+    path('cursos/Eliminar/<camada>',eliminar_curso,name="AppCoderEliminarCurso"),
+    path('curso/<nombre>/<camada>',crear_curso1,name="AppCoderCurso"),
     path('estudiantes',estudiantes,name="AppCoderEstudiantes"),
     path('profesores',profesores,name="AppCoderProfesores"),
 ]
